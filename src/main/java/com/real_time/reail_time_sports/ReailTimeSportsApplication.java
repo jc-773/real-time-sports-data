ca77c3d5392e67c6da6b0a0bb2b8795a019c9070
jc-773 <jonc960@gmail.com>
2025-02-20 21:05:52 -0500

    init commit

---
+++ src/main/java/com/real_time/reail_time_sports/ReailTimeSportsApplication.java
@@ -0,0 +1,13 @@
+package com.real_time.reail_time_sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReailTimeSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReailTimeSportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/real_time/reail_time_sports/ReailTimeSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_time.reail_time_sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReailTimeSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
