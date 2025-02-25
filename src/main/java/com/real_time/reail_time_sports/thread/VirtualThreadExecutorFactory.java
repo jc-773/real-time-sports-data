968e64fea3d7e396ef57b9ab799672c3332a253c
jc-773 <jonc960@gmail.com>
2025-02-25 10:07:57 -0500

    clean executeNbaGameServiceTask

--- src/main/java/com/real_time/reail_time_sports/thread/VirtualThreadExecutorFactory.java
+++ src/main/java/com/real_time/reail_time_sports/thread/VirtualThreadExecutorFactory.java
@@ -1,7 +1,6 @@
 package com.real_time.reail_time_sports.thread;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 import org.slf4j.Logger;
@@ -9,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import com.real_time.reail_time_sports.backend.NbaGameService;
 import com.real_time.reail_time_sports.model.PlayByPlayModel;
+
 
 public class VirtualThreadExecutorFactory {
     private static final Logger log = LoggerFactory.getLogger(VirtualThreadExecutorFactory.class);
@@ -18,18 +18,16 @@ public class VirtualThreadExecutorFactory {
         semaphore = new Semaphore(n);
     }
 
-    public CompletableFuture<PlayByPlayModel> executeNbaGameServiceTask(NbaGameService nbaGameService, String gameId) {
+    public CompletableFuture<PlayByPlayModel> executeNbaGameServiceTask(NbaGameService nbaGameService, String gameId) throws InterruptedException {
+        semaphore.acquire();
         try {
-            semaphore.acquire();
-            try(var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-                executor.submit(() -> {
-                    return nbaGameService.gamePlayByPlayRequestAsynchronous(gameId);
-                });
-            }
-            semaphore.release();
+            return nbaGameService.gamePlayByPlayRequestAsynchronous(gameId).toFuture();
         } catch (Exception e) {
             log.error("Exception: {}", e);
+            return CompletableFuture.failedFuture(e);
         }
-       return null;
+        finally {
+            semaphore.release();
+        } 
     }
 }
